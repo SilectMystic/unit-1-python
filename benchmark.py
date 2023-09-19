8843cceeb87c2a14762f7b87b3ee9d70f481ba47
SilectMystic <cvasquez@brooklynsteamcenter.org>
2023-09-19 09:51:33 -0400

    benchmark

---
+++ benchmark.py
@@ -0,0 +1,35 @@
+#Excercise 1: 
+print("Carlos")
+
+#Excercise 2:
+favoriteShow = "Suits"
+
+#Excercise 3:
+height = 5.5
+age = 17
+print(height * age)
+
+#Excercise 4:
+print("im going to break")
+
+#Excersice 5:
+fav_food = ["pizza", "hamburger", "meat"]
+
+#Excersice 6:
+if age >= 21:
+    print("You can rent a car")
+else:
+    print("your beat")
+
+#Excersice 7:
+a = input("What is your name: ")
+print("Hello " + a +"!")
+
+#Excersice 8:
+for foods in fav_food:
+    print(foods)
+
+#Excersice 9:
+b = 9
+while b == 8:
+    print("hola")
