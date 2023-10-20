1aa62b8b29a6ea7abe67837d20dce6211195227c
SilectMystic <cvasquez@brooklynsteamcenter.org>
2023-10-19 21:06:24 -0400

    work

--- classical_classes.py
+++ classical_classes.py
@@ -10,9 +10,9 @@ class Person():
         self.name = name
         self.age = age
     def hi(self):
-        print(self.name)
+        print('hi my name is ' + str(self.name) + ' and my age is ' + str(self.age))
 t1 = Person('pablo', 98)
-print(t1.hi)
+t1.hi()
 """
 Task 2: Animals Speak
 Create a base class Animal with a empty method called speak. 
@@ -21,8 +21,24 @@ Then create two subclasses, Dog and Cat, each with their own speak method.
 
 Create objects using these subclasses and call the speak method.
 """
+class Animal():
+    def speak(self):
+        print()
 
+class Dog(Animal):
+    def speak(self):
+        print('wuff')
+class Cat(Animal):
+    def speak(self):
+        print('meow')
 
+t2 = Animal()
+t2_2 = Dog()
+t2_3 = Cat()
+
+t2.speak()
+t2_2.speak()
+t2_3.speak()
 """
 Task 3: Banking
 Create a class BankAccount with attributes balance and owner. 
@@ -31,3 +47,8 @@ Include methods for depositing and withdrawing money, which should modify the ba
 
 Test these methods with instances of the class.
 """
+class BankAccount():
+    def __init__(self, balance, owner):
+        self.balance = balance
+        self.owner = owner
+    
