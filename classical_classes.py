1ba7518a4ed8ef67730680eaacb17c9243f118bb
SilectMystic <cvasquez@brooklynsteamcenter.org>
2023-10-19 23:55:49 -0400

    morework

--- classical_classes.py
+++ classical_classes.py
@@ -5,14 +5,15 @@ Then, write a method within the class to introduce the person with their name an
 
 Create a new object using this new class, and call the method you created.
 """
-class Person():
-    def __init__(self, name, age):
+class Person(): #creating class with the name person
+    def __init__(self, name, age): #innit method for name and age
         self.name = name
         self.age = age
-    def hi(self):
+    def hi(self): #method for printing their name and age
         print('hi my name is ' + str(self.name) + ' and my age is ' + str(self.age))
-t1 = Person('pablo', 98)
-t1.hi()
+t1 = Person('pablo', 98) #assigning class to a variable
+t1.hi() #printing the hi method
+print('------------------------------------------------------\n') #printing for separation
 """
 Task 2: Animals Speak
 Create a base class Animal with a empty method called speak. 
@@ -21,24 +22,26 @@ Then create two subclasses, Dog and Cat, each with their own speak method.
 
 Create objects using these subclasses and call the speak method.
 """
-class Animal():
-    def speak(self):
+class Animal(): #Initial class for template
+    def speak(self):#method called speak to print what they say
         print()
 
-class Dog(Animal):
-    def speak(self):
+class Dog(Animal): #Dog class using the animal template
+    def speak(self):#method called speak to print what they say
         print('wuff')
-class Cat(Animal):
-    def speak(self):
+
+class Cat(Animal):#Cat class using the animal template
+    def speak(self):#method called speak to print what they say
         print('meow')
 
-t2 = Animal()
+t2 = Animal() #Assigning all 3 classes to variables
 t2_2 = Dog()
 t2_3 = Cat()
 
-t2.speak()
+t2.speak() #Calling all variables to print their speak method 
 t2_2.speak()
 t2_3.speak()
+print('------------------------------------------------------\n')#printing for separation
 """
 Task 3: Banking
 Create a class BankAccount with attributes balance and owner. 
@@ -47,8 +50,23 @@ Include methods for depositing and withdrawing money, which should modify the ba
 
 Test these methods with instances of the class.
 """
-class BankAccount():
-    def __init__(self, balance, owner):
+class BankAccount(): #Creating class for bank account
+    def __init__(self, balance, owner): #innit method for all starter needed atributes
         self.balance = balance
         self.owner = owner
     
+    def total(self): #Method for printing total balance of bank
+        print('Your balance is ' + str(self.balance))
+
+    def deposit(self, depo): #method for adding deposit and printing total value
+        self.balance = self.balance + depo
+        print('Your total balance is ' + str(self.balance))
+
+    def withdraw(self, withd): #method for substracting the withdraw then printing total value
+        self.balance = self.balance - withd
+        print('Your total balance is ' + str(self.balance))
+
+t3_bank = BankAccount(500, 'a') #assigning class to variable
+t3_bank.total() #printing total method for balance
+t3_bank.deposit(200)#using deposit method for adding and printing balance
+t3_bank.withdraw(100)#using withdraw method for substracting and printing balance
